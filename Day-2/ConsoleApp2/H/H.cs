95f6169a64511bfc00f5194e62e8db02dfa5098d
maryam <mm5252@fayoum.edu.eg>
2025-02-20 13:46:39 +0200

    add problem H

---
+++ Day-2/ConsoleApp2/H/H.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace H
+{
+    class H
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            long number1 = long.Parse(inputs[0]);
+            long number2 = long.Parse(inputs[1]);
+
+            Console.WriteLine($"floor {number1} / {number2} = {number1 / number2}");
+            Console.WriteLine($"ceil {number1} / {number2} = {(number1 + number2 - 1) / number2}");
+            Console.WriteLine($"round {number1} / {number2} = {Math.Round((double)number1 / number2, MidpointRounding.AwayFromZero)}");
+        }
+    }
+}
