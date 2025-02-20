4fc80bfa3792ad75baab80e8969d1ff15ea66ac6
maryam <mm5252@fayoum.edu.eg>
2025-02-20 13:56:33 +0200

    add problem J

---
+++ Day-2/ConsoleApp2/J/J.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace I
+{
+
+    class I
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            long number1 = long.Parse(inputs[0]);
+            long number2 = long.Parse(inputs[1]);
+            
+            if(number1 %  number2 == 0 || number2 % number1 == 0)
+                Console.WriteLine("Multiples");
+            else
+                Console.WriteLine("No Multiples");
+            
+                
+            
+        }
+    }
+}
