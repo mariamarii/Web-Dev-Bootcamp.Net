4d2ffab18caf45940823adfe2f88ef73977ae6ca
maryam <mm5252@fayoum.edu.eg>
2025-02-20 13:51:28 +0200

    add problem I

---
+++ Day-2/ConsoleApp2/I/I.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace I
+{
+
+    class I
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            long number1 = long.Parse(inputs[0]);
+            long number2 = long.Parse(inputs[1]);
+            
+            if(number1 >=  number2)
+                Console.WriteLine("Yes");
+            else
+            
+                Console.WriteLine("No");
+            
+        }
+    }
+}
