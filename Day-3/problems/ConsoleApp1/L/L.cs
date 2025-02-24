f50262469eb05cc0a4569e0c3e752dd102206b0d
maryam <mm5252@fayoum.edu.eg>
2025-02-25 01:00:30 +0200

    problem L

---
+++ Day-3/problems/ConsoleApp1/L/L.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L
+{
+    class L
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            string father1 = inputs[1];
+            input = Console.ReadLine();
+            inputs = input.Split(' ');
+            string father2 = inputs[1];
+            
+            if (father1 == father2)
+                Console.WriteLine("ARE Brothers");
+            else
+            {
+                Console.WriteLine("NOT");
+            }
+
+  
+        }
+    }
+}
